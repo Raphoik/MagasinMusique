dfaac03c0e66126e6b0412651fbdc509f9648788
Gabriel Cardinal <gabrielc@outlook.fr>
2016-11-23 21:56:01 -0500

    Delete Instrument.java

--- Instrument.java
+++
@@ -1,8 +0,0 @@
-public abstract class Instrument extends Item
-{
-    private String marque;
-    private String model;
-    private int annee;
-    private String couleur;
-    private String type;
-}
